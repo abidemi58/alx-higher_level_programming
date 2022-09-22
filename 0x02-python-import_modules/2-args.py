cad10da5cb5e3339a0c465091be4dbd6188c7f48
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-22 16:34:53 -0700

    lists of areguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_arg = len(sys.argv)
+    if num_arg = 1:
+        print("{} argument:".format(num_arg -1)
+    elif num == 2:
+        print("{} argument:".format(num_arg - 1))
+    else:
+        print("{} arguments:".format(num_arg - 1))
+
+    for i in range(1, num_arg):
+        print("{}: {}".format(i, sys.argv[i]))
