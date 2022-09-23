b345c201a3007ee0cc09ef72cd78a972c57c50da
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-22 18:56:35 -0700

    basic op

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    if len(sys.argv) - 1 != 3:
+    num_args = len(sys.argv)
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
+        exit(1)
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
