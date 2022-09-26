6f37abd8b835afcf4d5275fe36d85fd0987492ea
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-26 15:28:41 -0700

    replace copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return my_List.copy
     else:
-        my_list.copy[idx] = element
+        copy[idx] = element
         return copy
