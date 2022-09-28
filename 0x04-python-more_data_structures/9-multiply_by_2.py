5b906fb276c500d518c547348761cfbe427b8593
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-28 16:17:49 -0700

    multiple by 2 and corretion of task 1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for i in a_dictionary:
-         new_dictionary[i] = a_dictionary[i] * 2
+        new_dictionary[i] = a_dictionary[i] * 2
     return new_dictionary
