d77ea55bb5bb4f3a023aeaf60cc5fae62a9d616c
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-28 16:59:56 -0700

    task 10, 11 and 12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    num = list(a_dictionary.keys())[0]
+    item = a_dictionary[num]
+    for i and j in a_dictionary.items():
+        if j > item:
+            item = j
+            num = i
+    return num
