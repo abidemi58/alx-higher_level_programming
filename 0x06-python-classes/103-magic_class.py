948d6e9d4163ed169c62ab9790ed3e111eea263c
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-10-12 15:43:39 -0700

    all files updated

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -20,4 +20,4 @@ class MagicClass:
 
     def circumference(self):
         """Calculation of the circumference."""
-        return 2 * math.pi * self._MagicClass__radiu s
+        return 2 * math.pi * self._MagicClass__radius
