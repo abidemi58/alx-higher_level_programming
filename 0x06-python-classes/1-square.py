bcfaecef1e4639f1caae69bec145d88054a765da
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-10-12 13:19:22 -0700

    all file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """Represent a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+    def __init__(self, size):
+        """Initialize a new Square."""
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with optional size.
+    """
+    def __init__(self, size=0):
+        """Initializes a new Square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
